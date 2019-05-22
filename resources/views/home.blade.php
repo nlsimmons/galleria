3e1959bd6cdfde5765af5f99fbc03a4b3ba4b55d
Nathaniel Simmons <nlsimmons42@gmail.com>
2019-05-22 15:57:25 -0600

    Primary layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,31 @@
 
 <div class="section">
 
-    <div class="container">
+    <div class="container-fluid">
+        <h1 class="title is-1">All Photos</h1>
 
+        <div id="photos" style="display: flex">
+
+            <img src="http://via.placeholder.com/800x600">
+            <img src="http://via.placeholder.com/800x600">
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="section">
+
+    <div class="container-fluid">
+        <h1 class="title is-1">Albums</h1>
+
+        <div id="albums" style="display: flex">
+
+            <img src="http://via.placeholder.com/800x600">
+            <img src="http://via.placeholder.com/800x600">
+
+        </div>
     </div>
 
 </div>
