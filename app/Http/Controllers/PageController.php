d5298f4d21f930435d15f7bf43febf2ebb20cdc2
Nathaniel Simmons <nlsimmons42@gmail.com>
2019-06-02 02:11:49 -0600

    Move storage method to App\Image

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\File;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
-use ImageManager;
-
 use App\Image;
 use App\Slideshow;
 
@@ -45,7 +43,7 @@ class PageController extends Controller
         $files = $request->file('image-no-album');
         foreach( $files as $file )
         {
-            $this->storeFile($file);
+            Image::upload( $file, Auth::id() );
         }
 
         return redirect('home');
@@ -55,51 +53,9 @@ class PageController extends Controller
     {
         if( $request->has('delete') )
         {
-            $this->delete($request->delete);
+            Image::destroy($request->delete);
         }
 
         return $this->home();
-    }
-
-    protected function delete($id)
-    {
-        $image = Image::find($id);
-
-        if($image)
-        {
-            Storage::delete([
-                $image->url,
-                $image->display_url,
-                $image->thumbnail_url
-            ]);
-
-            Image::destroy($id);
-        }
-    }
-
-    protected function storeFile( \Illuminate\Http\UploadedFile $file )
-    {
-        $name = md5($file);
-        $img = ImageManager::make($file);;
-
-        Storage::put(
-            $main_img = 'public/images/' . $name . '.jpg',
-            $img->encode('jpg')
-        );
-        Storage::put(
-            $display_img = 'public/images/' . $name . '_display.jpg',
-            $img->widen(500)->encode('jpg')
-        );
-        Storage::put(
-            $thumb_img = 'public/images/' . $name . '_thumb.jpg',
-            $img->widen(100)->encode('jpg')
-        );
-
-        $image = new Image;
-        $image->url = $main_img;
-        $image->display_url = $display_img;
-        $image->thumbnail_url = $thumb_img;
-        $image->owner = Auth::id();
-        $image->save();
     }
 }
--- app/Image.php
+++ app/Image.php
@@ -2,8 +2,55 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use ImageManager;
 
 class Image extends Model
 {
-    //
+	private static $display_size = 500;
+	private static $thumb_size = 100;
+
+    public static function upload( \Illuminate\Http\UploadedFile $file, $owner_id )
+    {
+    	$name = md5($file);
+        $img = ImageManager::make($file);;
+
+        Storage::put(
+            $main_img = 'public/images/' . $name . '.jpg',
+            $img->encode('jpg')
+        );
+        Storage::put(
+            $display_img = 'public/images/' . $name . '_display.jpg',
+            $img->widen( self::$display_size )->encode('jpg')
+        );
+        Storage::put(
+            $thumb_img = 'public/images/' . $name . '_thumb.jpg',
+            $img->widen( self::$thumb_size )->encode('jpg')
+        );
+
+        $new = new self;
+        $new->url = $main_img;
+        $new->display_url = $display_img;
+        $new->thumbnail_url = $thumb_img;
+        $new->owner = $owner_id;
+        $new->save();
+
+        return $new;
+    }
+
+    public static function destroy($id)
+    {
+    	$image = self::find($id);
+
+        if($image)
+        {
+            Storage::delete([
+                $image->url,
+                $image->display_url,
+                $image->thumbnail_url
+            ]);
+
+            return parent::destroy($id);
+        }
+    }
 }
