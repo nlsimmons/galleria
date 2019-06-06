92e9f16757433204dfcaa9a1c5333f22fda899f7
Nathaniel Simmons <nlsimmons42@gmail.com>
2019-06-06 00:22:09 -0600

    Styling

--- resources/views/components/slideshow/slides.blade.php
+++ resources/views/components/slideshow/slides.blade.php
@@ -1,58 +1,18 @@
 @foreach( $slides as $slide )
-    @if($slide->item instanceof Illuminate\Database\Eloquent\Model)
 
-    <div class="carousel-slide carousel-slide-{{ $slide->id }}">
+        {{-- <div class="carousel-slide carousel-slide-{{ $slide->id }}">
 
-        <p id="title_{{ $type }}_{{ $slide->id }}" contenteditable class="title slide-title {{ $slide->title ? '' : 'placeholder'  }}">
-            {{ $slide->title ?: 'Click to add a title' }}
-        </p>
+            <p id="title_{{ $type }}_{{ $slide->id }}" contenteditable class="title slide-title {{ $slide->title ? '' : 'placeholder'  }}">
+                {{ $slide->title ?: 'Click to add a title' }}
+            </p>
 
-        <div class="slide_image_wrapper display_wrapper" id="display_{{ $type }}_{{ $slide->id }}">
-            <img class="display_image" src="{{ asset($slide->display_url) }}">
-        </div>
+            <div class="slide_image_wrapper display_wrapper" id="display_{{ $type }}_{{ $slide->id }}">
+                <img class="display_image" src="{{ asset($slide->display_url) }}">
+            </div>
 
-        @if( !empty($buttons) )
-            <form class="box carousel-slide-control" name="slide-action" method="post">
-                @csrf
+        </div> --}}
 
-                @if(in_array('delete', $buttons))
-                    <button class="button button-no-border" name="delete" title="Delete" value="{{ $slide->id }}">
-                        <i class="fas fa-minus-circle"></i>
-                    </button>
-                @endif
-
-                @if(in_array('download', $buttons))
-                    <a class="button button-no-border" name="download" title="Download"
-                       href="{{ $slide->download_link }}"
-                       value="{{ $slide->id }}">
-                        <i class="fas fa-arrow-alt-circle-down"></i>
-                    </a>
-                @endif
-
-                @if(in_array('edit', $buttons))
-                    <button class="button button-no-border" name="edit" title="Edit" value="{{ $slide->id }}">
-                        <i class="fas cs cs-edit-circle"></i>
-                    </button>
-                @endif
-
-                @if(in_array('tags', $buttons))
-                    <span class="button button-no-border">
-                        <i class="fas cs cs-tags"></i>
-                    </span>
-                @endif
-
-                @if(in_array('album', $buttons))
-                    <span class="button button-no-border">
-                        <i class="fas cs cs-album"></i>
-                    </span>
-                @endif
-
-            </form>
-        @endif
-
-    </div>
-
-    @else
+    @if( !( $slide->item instanceof Illuminate\Database\Eloquent\Model ) )
 
         @if($type == 'image')
 
@@ -79,5 +39,60 @@
 
         @endif
 
+
+    @else
+
+        <div class="carousel-slide">
+
+            <p id="title_{{ $type }}_{{ $slide->id }}" contenteditable class="title slide-title {{ $slide->title ? '' : 'placeholder'  }}">
+                {{ $slide->title ?: 'Click to add a title' }}
+            </p>
+
+            <div class="slide_image_wrapper display_wrapper" id="display_{{ $type }}_{{ $slide->id }}">
+                <img class="display_image" src="{{ asset($slide->display_url) }}">
+            </div>
+
+            @if( !empty($buttons) )
+                <form class="box carousel-slide-control" name="slide-action" method="post">
+                    @csrf
+
+                    @if(in_array('delete', $buttons))
+                        <button class="button button-no-border" name="delete" title="Delete" value="{{ $slide->id }}">
+                            <i class="fas fa-minus-circle"></i>
+                        </button>
+                    @endif
+
+                    @if(in_array('download', $buttons))
+                        <a class="button button-no-border" name="download" title="Download"
+                           href="{{ $slide->download_link }}"
+                           value="{{ $slide->id }}">
+                            <i class="fas fa-arrow-alt-circle-down"></i>
+                        </a>
+                    @endif
+
+                    @if(in_array('edit', $buttons))
+                        <button class="button button-no-border" name="edit" title="Edit" value="{{ $slide->id }}">
+                            <i class="fas cs cs-edit-circle"></i>
+                        </button>
+                    @endif
+
+                    @if(in_array('tags', $buttons))
+                        <span class="button button-no-border">
+                            <i class="fas cs cs-tags"></i>
+                        </span>
+                    @endif
+
+                    @if(in_array('album', $buttons))
+                        <span class="button button-no-border">
+                            <i class="fas cs cs-album"></i>
+                        </span>
+                    @endif
+
+                </form>
+            @endif
+
+        </div>
+
     @endif
+
 @endforeach
